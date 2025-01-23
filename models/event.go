bdd582553092b2df7ec45fb665a1cd41449c17e8
Tshegofatso-12 <62068842+Tshegofatso12-ai@users.noreply.github.com>
2025-01-23 16:35:34 +0200

    add routes to fetch and save events to db

--- models/event.go
+++ models/event.go
@@ -1,9 +1,13 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"example.com/event-booking/db"
+)
 
 type Event struct {
-	ID          int
+	ID          int64
 	Name        string    `binding:"required"`
 	Description string    `binding:"required"`
 	Location    string    `binding:"required"`
@@ -13,10 +17,56 @@ type Event struct {
 
 var events []Event = []Event{}
 
-func (e Event) Save() {
-	events = append(events, e)
+func (e Event) Save() error {
+	query := `
+	INSERT into events (name, description, location, dateTime, user_id)
+	VALUES (?,?,?,?,?)`
+	statement, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer statement.Close()
+	result, err := statement.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+	e.ID = id
+	return err
 }
 
-func GetAllEvents() []Event {
-	return events
+func GetAllEvents() ([]Event, error) {
+	query := "SELECT * FROM events"
+	rows, err := db.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var events []Event
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+		if err != nil {
+			return nil, err
+		}
+		events = append(events, event)
+	}
+
+	return events, nil
+}
+
+func FetchEventByID(id int64) (*Event, error) {
+	query := "SELECT * FROM events WHERE id = ?"
+
+	row := db.DB.QueryRow(query, id)
+
+	var event Event
+	err := row.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &event, nil
 }
---
+++ routes/events.go
@@ -0,0 +1,49 @@
+package routes
+
+import (
+	"net/http"
+	"strconv"
+
+	"example.com/event-booking/models"
+	"github.com/gin-gonic/gin"
+)
+
+func getEvents(context *gin.Context) {
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message:": "Could not fetch events"})
+		return
+	}
+	context.JSON(http.StatusOK, events)
+}
+
+func getEvent(context *gin.Context) {
+	id, err := strconv.ParseInt(context.Param("eventID"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadGateway, gin.H{"message:": "Could parse event id"})
+		return
+	}
+	event, err := models.FetchEventByID(id)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message:": "Could not fetch event"})
+		return
+	}
+	context.JSON(http.StatusOK, event)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindBodyWithJSON(&event)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message:": "Could not parse request"})
+		return
+	}
+	event.ID = 1
+	event.UserID = 1
+	err = event.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message:": "Could not create event"})
+		return
+	}
+	context.JSON(http.StatusCreated, gin.H{"message:": "Event created", "event": event})
+}
